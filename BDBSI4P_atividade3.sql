5b978b96047ad479e601b4c29e8286de65105864
Jackson Willian <43284155+jacksonwillian@users.noreply.github.com>
2019-04-02 14:56:44 -0300

    Altera atividade 3
    Acréscimo de comandos sql

--- BDBSI4P_atividade3.sql
+++ BDBSI4P_atividade3.sql
@@ -13,4 +13,8 @@ select avg(salario), sum(salario),max(salario), min(salario),stddev(salario) fro
 /* PARTE 2 */
 
 select empregado.nome_completo, count(dependente.rg_responsavel) from empregado inner join dependente on (dependente.rg_responsavel = empregado.rg) group by dependente.rg_responsavel, empregado.nome_completo;
-
+select empregado.nome_completo, count(empregado_projeto.rg_empregado) from empregado inner join empregado_projeto on (empregado_projeto.rg_empregado = empregado.rg) group by empregado.nome_completo, empregado_projeto.rg_empregado;
+select empregado.nome_completo, empregado_projeto.rg_empregado, sum(empregado_projeto.horas) as horas_tranalhadas from empregado inner join empregado_projeto on (empregado_projeto.rg_empregado = empregado.rg) group by empregado.nome_completo,empregado_projeto.rg_empregado order by horas_tranalhadas desc;
+select departamento.nome_departamento,  count(empregado.depto) as quant_funcionarios from departamento inner join empregado on (departamento.numero = empregado.depto) group by departamento.nome_departamento,  empregado.depto
+select departamento.nome_departamento,  count(empregado.depto) as quant_funcionarios from departamento inner join empregado on (departamento.numero = empregado.depto) group by departamento.nome_departamento,  empregado.depto having  count(empregado.depto) >= 2
+select departamento.nome_departamento, sum(empregado.salario) from departamento inner join empregado on (departamento.numero = empregado.depto) group by departamento.nome_departamento, empregado.depto having sum(empregado.salario) > 6000;
