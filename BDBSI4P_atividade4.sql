228b0f3c56e036a9af0c7020af275e4116b8d13a
Jackson Willian <43284155+jacksonwillian@users.noreply.github.com>
2019-04-13 12:15:47 -0300

    Atualiza atividade4 sql

--- BDBSI4P_atividade4.sql
+++ BDBSI4P_atividade4.sql
@@ -11,4 +11,21 @@ create view soma_salario_empregados as select sum(empregado.salario) from empreg
 create view tres_primeiros_empregados as select * from empregado order by empregado.salario desc limit 3;
 create view empregados_sem_supervisor as select emp.rg, emp.cpf,emp.rg_supervisor,emp.nome_completo from empregado as emp where emp.rg_supervisor is null;
 create view empregados_com_historico_Salario as select distinct emp.nome_completo from empregado as emp inner join historico_salario as histsal on (histsal.rg = emp.rg);
-create view juncao_depproj_proj as select * from departamento_projeto as depproj right outer join projeto as proj on (proj.numero = depproj.numero_projeto);
+create view depproj_proj as select depproj.*, proj.numero as numero_do_projeto, proj.nome_do_projeto, proj.localidade from departamento_projeto as depproj right outer join projeto as proj on (proj.numero = depproj.numero_projeto);
+
+/* join esquerda e direita */
+
+select * from departamento_projeto as depproj right outer join projeto as proj on (depproj.numero_projeto = proj.numero);
+select emp.nome_completo as funcionario, empproj.numero_projeto from empregado as emp left outer join empregado_projeto as empproj on (empproj.rg_empregado = emp.rg);
+select emp.nome_completo as funcionario, proj.nome_do_projeto from empregado as emp left outer join empregado_projeto as empproj on (empproj.rg_empregado = emp.rg) left outer join projeto as proj on (empproj.numero_projeto = proj.numero);
+select emp.nome_completo as funcionario, histsal.dat_ini_sal, histsal.dat_fim_sal, histsal.salario_mensal from empregado as emp left outer join historico_salario as histsal on (histsal.rg = emp.rg);
+select emp.nome_completo as funcionario, sum( empproj.horas) as horas_projetos from empregado as emp left outer join empregado_projeto as empproj on (empproj.rg_empregado = emp.rg) group by  emp.nome_completo  order by emp.nome_completo;
+select emp.nome_completo as funcionario, proj.nome_do_projeto as nome_projeto, empproj.horas as horas_trabalhadas from empregado as emp inner join empregado_projeto as empproj on (empproj.rg_empregado = emp.rg) inner join  projeto as proj on (empproj.numero_projeto = proj.numero) where proj.numero <> 5;
+select emp.nome_completo as funcionario, dep.nome_departamento, hs.* from empregado as emp full join departamento as dep on (emp.depto = dep.numero) left outer join historico_salario as hs on (hs.rg = emp.rg);
+select emp.nome_completo as funcionario, dep.nome_departamento, hs.* from empregado as emp full join departamento as dep on (emp.depto = dep.numero) left outer join historico_salario as hs on (hs.rg = emp.rg) order by hs.salario_mensal desc ;
+
+/* Limit e offset */
+
+select * from dependente limit 5 ;
+select * from dependente limit 3 offset 3;
+select * from empregado order by empregado.salario desc limit 3;
